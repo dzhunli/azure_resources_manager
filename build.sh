cc6212ffa02b91099045ab5fb52f1efcaa7aab2f
dzhun <dzhun@itstep.org>
2024-07-22 13:02:13 +0300

    rm unused

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-rm subs_manager_GUI || true
-cython $1.py --embed
-PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags)
-gcc -Os $(python3-config --includes) $1.c -o GUI/subs_manager $(python3-config --ldflags) -l$PYTHONLIBVER
-rm $1.c
